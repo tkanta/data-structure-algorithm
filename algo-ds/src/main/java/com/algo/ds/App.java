c4c812ccc47e36c7e71a4799fa8849de7cdb1d88
tarini <tarininegi@gmail.com>
2020-10-03 19:29:10 +0530

    init algo-ds

---
+++ algo-ds/src/main/java/com/algo/ds/App.java
@@ -0,0 +1,13 @@
+package com.algo.ds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
