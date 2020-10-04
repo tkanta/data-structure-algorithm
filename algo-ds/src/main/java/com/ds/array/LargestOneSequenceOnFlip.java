0102f59404b7cb851c4e5f328bf5069bad22ea87
tarini <tarininegi@gmail.com>
2020-10-04 18:54:06 +0530

    largest one's sequence on flip

---
+++ algo-ds/src/main/java/com/ds/array/LargestOneSequenceOnFlip.java
@@ -0,0 +1,78 @@
+package com.ds.array;
+
+/**
+ * Find longest sequence of 1's with one flip
+ * @author tarin
+ *
+ */
+public class LargestOneSequenceOnFlip {
+
+	private static int[] arrInt = {1,1,1,0,1,1,1,0,1,1,1,1,1};
+	
+	public static void main(String[] args) {
+		
+		int x = 0;
+		int y = 0;
+		boolean isZero = false;
+		int maxSeq = 0;
+		
+		for(int i = 0; i<arrInt.length; i++) {
+			
+			if(arrInt[i] == 1) {
+				if(isZero) y++; else x++;
+				
+				if(i == arrInt.length-1) {
+					if(x+y+1 > maxSeq && isZero) {
+						maxSeq = x+y+1;
+					}else {
+						maxSeq = x;
+					}
+				}
+			}
+			
+			if(arrInt[i] == 0) {
+				isZero = true;
+				if(i == arrInt.length-1) {
+					maxSeq = x+y+1;
+				}else if(x>0 && y>0) {
+					if(x+y+1 > maxSeq) {
+						maxSeq= x+y+1;
+					}
+					//reset
+					x = y;
+					y = 0;
+				}
+			}
+			
+		}
+		
+		System.out.println(maxSeq);
+		
+	}
+	
+	
+//	private static void findMaxSeq() {
+//		int sequenceOnesTot = 0;
+//		ArrayList<Integer> list = new ArrayList<>();
+//		
+//		for(int i = 0; i < arrInt.length; i++) {
+//			
+//			if(arrInt[i] == 1) {
+//				sequenceOnesTot++;
+//				if(sequenceOnesTot > 0 && i == arrInt.length-1) {
+//					list.add(sequenceOnesTot);
+//				}
+//			}else {
+//				if(sequenceOnesTot > 0) {
+//					list.add(sequenceOnesTot);
+//				}
+//				list.add(arrInt[i]);
+//				sequenceOnesTot = 0;
+//			}
+//			
+//		}
+//		System.out.println(list);
+//		int maxSeq = 0;
+//	}
+	
+}
